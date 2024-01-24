57c2d58e0d7fe21f8dc442dad14093f8f5f92345
Kyrios0 <kyr1os@qq.com>
2024-01-24 03:38:47 -0800

    Add application slice

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./reducers/user";
 import registrationSlice from "./reducers/registration";
+import applicationSlice from "./reducers/registration";
 
 const store = configureStore({
   reducer: {
     user: userSlice.reducer,
     registration: registrationSlice.reducer,
+    application: applicationSlice.reducer,
   },
 });
 
