089a2418dd79aac069d9f641e8b1c4be47616cc5
Shan Qing <sqing73@gmail.com>
2024-01-25 15:05:47 -0800

    update list employee profiles to return more fields

--- backend/controllers/employeeProfileController.js
+++ backend/controllers/employeeProfileController.js
@@ -2,21 +2,21 @@ const EmployeeProfile = require("../models/employeeProfile");
 
 exports.getAllEmployeeProfiles = async (req, res) => {
   try {
-    const employees = await EmployeeProfile.find(
+    const employeeProfiles = await EmployeeProfile.find(
       {},
-      "name personalInfo.ssn residencyStatus.status phoneNumbers email"
+      "-VisaStatus"
     );
-    const formattedEmployees = employees.map((emp) => ({
-      id: emp._id, // Including the ObjectId
-      name: `${emp.name.first} ${emp.name.middle ? emp.name.middle + " " : ""}${
-        emp.name.last
-      }`,
-      ssn: emp.personalInfo.ssn,
-      workAuthorizationTitle: emp.residencyStatus.status,
-      phoneNumber: emp.phoneNumbers.cell, // or work, depending on what you need
-      email: emp.email,
-    }));
-    res.json(formattedEmployees);
+    // const formattedEmployees = employees.map((emp) => ({
+    //   id: emp._id, // Including the ObjectId
+    //   name: `${emp.name.first} ${emp.name.middle ? emp.name.middle + " " : ""}${
+    //     emp.name.last
+    //   }`,
+    //   ssn: emp.personalInfo.ssn,
+    //   workAuthorizationTitle: emp.residencyStatus.status,
+    //   phoneNumber: emp.phoneNumbers.cell, // or work, depending on what you need
+    //   email: emp.email,
+    // }));
+    res.status(200).json(employeeProfiles);
   } catch (error) {
     res.status(500).send(error.message);
   }
