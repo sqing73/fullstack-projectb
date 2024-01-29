885ac8acb5f46d9b6540d452aa14eccaba303a1e
Kyrios0 <kyr1os@qq.com>
2024-01-28 21:16:11 -0800

    Update store.js
    Resolve conflict

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./reducers/user";
 import registrationSlice from "./reducers/registration";
 import applicationSlice from "./reducers/registration";
+import profileSlice from "./reducers/profile";
 
 const store = configureStore({
   reducer: {
     user: userSlice.reducer,
     registration: registrationSlice.reducer,
     application: applicationSlice.reducer,
+    profile: profileSlice.reducer,
   },
 });
 
